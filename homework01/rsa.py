befc0f419573819f3472439d869d1f7cdeae5661
Matvei-Fadeev <krx@matwei.fadeew@yandex.ru>
2020-10-29 14:48:08 +0300

    Implemented is_prime function

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -13,7 +13,13 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
+    if n < 2:
+        return False
+    for i in range(2, n - 1):
+        if n % i == 0:
+            return False
+    return True
+
     pass
 
 
